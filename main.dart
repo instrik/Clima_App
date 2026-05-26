8f396f73c730ccb31fa9ad7f3223f5c7a03c7d24
Aakash Agarwal <agarwal7409@gmail.com>
2026-05-26 22:27:13 +0530

    Refactor main.dart to remove commented code

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-// trial comment 7
+// trial comment 8
 
 void main() {
   runApp(AppBase());
