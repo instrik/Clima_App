6a2c7c72b74f0dcbb54ebdff4e99733a22c315e9
Aakash Agarwal <agarwal7409@gmail.com>
2026-05-28 01:37:29 +0530

    Restore main.dart structure and comments
    Restored import statements and main function. Updated trial comment.

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-// trial comment 12345
+// trial comment 123456
 
 void main() {
   runApp(AppBase());
