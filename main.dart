9ffba62946a81136c58ce48dcd1e8a3bbf8d2420
Aakash Agarwal <agarwal7409@gmail.com>
2026-06-15 01:41:38 +0530

    Refactor imports and clean up main.dart

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 kjnmik
-// trial comment ghknjnhijnmkjmk
+// trial comment ghknjnhijnmkjmklml
 
 void main() {
   runApp(AppBase());
