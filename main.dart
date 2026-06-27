d9abf8ccc2c84a3b2baf0bc362fab8857e496896
Aakash Agarwal <agarwal7409@gmail.com>
2026-06-27 23:19:07 +0530

    Restore imports and main structure in main.dart

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-// trial comment vujm;k
+// trial comment vujm;kklkk
 
 void main() {
   runApp(AppBase());
