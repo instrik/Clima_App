bb731b0142334f061dfd9c18e675a460c1b0978a
Aakash Agarwal <agarwal7409@gmail.com>
2026-07-02 00:28:15 +0530

    Restore imports and main app structure

--- main.dart
+++ main.dart
@@ -11,9 +11,9 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-df
-// trial commentk;jkjhjkjkkjkjfgf'jklmmnklknkljkn
-jkkhjklkui
+
+// trial comment
+
 void main() {
   runApp(AppBase());
 }
