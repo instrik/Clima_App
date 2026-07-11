4f5af713d1f56eb1923c30154eb0d4b29b5b3c21
Aakash Agarwal <agarwal7409@gmail.com>
2026-07-12 00:01:11 +0530

    Restore main.dart with initial content and imports

--- main.dart
+++ main.dart
@@ -13,7 +13,7 @@ import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // trial commentkjjhkjkn.mkj.jkjkjkn.kjk.
-kjk.kljn
+kjk.kljnkjnm
 void main() {
   runApp(AppBase());
 }
