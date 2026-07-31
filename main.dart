03f30101b4d82475902c60aa2a441779968d04d5
Aakash Agarwal <aakash@instrik.com>
2026-07-31 23:39:00 +0530

    Refactor imports and clean up main.dart

--- main.dart
+++ main.dart
@@ -11,7 +11,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'getLocation.dart';
 
-//trial comment sdfdgdfghfcv cvdsfd
+//trial comment sdfdgdfghfcv cvdsfddcfvb
 
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
