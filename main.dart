b6cfd5b368bb697e5391f5b608106704251094a8
Aakash Agarwal <aakash@instrik.com>
2026-08-31 00:06:35 +0530

    Restore imports and main app structure

--- main.dart
+++ main.dart
@@ -13,7 +13,7 @@ import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 //trial comment jhkljhkjhkjhkjhkjhjkhjkhkjh
-kjhkijukh
+kjhkijukhkjh
 void main() {
   runApp(AppBase());
 }
