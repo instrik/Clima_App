68b6dabc28c2d05d7525502b11325461e70e742d
Aakash Agarwal <aakash@instrik.com>
2026-09-24 18:33:22 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,8 +12,8 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-//trial comment jkhjhjhjiuijhjkh
-khjjh
+//trial comment jkh
+
 void main() {
   runApp(AppBase());
 }
