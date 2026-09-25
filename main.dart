a59582f790e0cc364490586cb439fbb14f07ff7b
Aakash Agarwal <aakash@instrik.com>
2026-09-25 23:53:18 +0530

    Refactor main.dart by restoring imports and structure

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart';
 import 'getLocation.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 dfdf
-//trial comment cfgbiuyukjhj
+//trial comment cfgbiuyukjhjklj
 
 void main() {
   runApp(AppBase());
